618cba2118273e547dc38340002b30abb3e6d3e2
XiDianMouse <xidian_guobenhao@163.com>
2017-05-24 10:44:46 +0800

    系统级常量

--- app/src/main/java/com/safehome/app/AppConstants.java
+++ app/src/main/java/com/safehome/app/AppConstants.java
@@ -12,4 +12,6 @@ public class AppConstants {
     public static final int STATE_EMPTY = 3;
     public static final int STATE_SUCCESS = 4;
 
+    public static final String KEY_API="a6f4c280091335854737abe264dbdf3d";
+    public static  final int WECHA_SEARCH = 1000;
 }
