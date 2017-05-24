3108c884979985a8b181cd5450ced534f3b559ab
XiDianMouse <xidian_guobenhao@163.com>
2017-05-24 11:46:49 +0800

    指纹识别控制Fragment

--- app/src/main/java/com/safehome/ui/fragment/home/child/PageHandFragment.java
+++ app/src/main/java/com/safehome/ui/fragment/home/child/PageHandFragment.java
@@ -1,14 +1,19 @@
 package com.safehome.ui.fragment.home.child;
 
+import android.animation.PropertyValuesHolder;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
 import android.graphics.drawable.StateListDrawable;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.blankj.utilcode.utils.ToastUtils;
 import com.safehome.R;
 import com.safehome.ui.activity.main.HomeActivity;
 import com.safehome.utils.MySelectorUtils;
+
+import java.util.concurrent.FutureTask;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -19,10 +24,23 @@ import butterknife.OnClick;
  */
 
 public class PageHandFragment extends BaseHomeFragment {
-    @BindView(R.id.hand_open)
-    ImageButton handOpen;
     @BindView(R.id.hand_showstatus)
     TextView handShowstatus;
+
+    @BindView(R.id.hand_open)
+    ImageView handOpen;
+
+    @BindView(R.id.hand_register)
+    ImageView handRegister;
+
+    @BindView(R.id.hand_identification)
+    ImageView handIdentification;
+
+    @BindView(R.id.hand_delete)
+    ImageView handDelete;
+
+    @BindView(R.id.hand_deleteall)
+    ImageView handDeleteall;
 
     private StateListDrawable openStateListDrawable;
     private StateListDrawable closeStateListDrawable;
@@ -58,42 +76,46 @@ public class PageHandFragment extends BaseHomeFragment {
     protected void onFragmentVisibleChange(boolean isVisible) {
         super.onFragmentVisibleChange(isVisible);
         if (isVisible) {//do things when fragment is visible
-            CURRENT_FRAGMENT = 0;
         } else {
         }
     }
 
-    @OnClick({R.id.hand_open, R.id.hand_delete, R.id.hand_register, R.id.hand_identification, R.id.hand_deleteall})
+    @OnClick({R.id.ll_hand_register, R.id.ll_hand_identification, R.id.ll_hand_open, R.id.ll_hand_delete, R.id.ll_hand_deleteall})
     public void onViewClicked(View view) {
-        switch(HomeActivity.commandStyle) {
-			case 0:
-				sendCmdByBluetooth(view);
-				break;
-			case 1:
-				sendCmdByGPRS(view);
-				break;
-			default:
+        setAnimator(view);
+        switch (mCommandStyle) {
+            case 0:
+                sendCmdByBluetooth(view);
+                break;
+            case 1:
+                sendCmdByGPRS(view);
+                break;
+            case 2:
+                sendCmdByNetwork(view);
+                break;
+            default:
                 ToastUtils.showShortToast("请选择命令发送方式");
-		}
+        }
     }
+
 
     private void sendCmdByBluetooth(View v) {
         String cmd = null;
         String data = "0000";
         switch (v.getId()) {
-            case R.id.hand_delete:
+            case R.id.ll_hand_delete:
                 cmd = "01";
                 break;
-            case R.id.hand_deleteall:
+            case R.id.ll_hand_deleteall:
                 cmd = "02";
                 break;
-            case R.id.hand_register:
+            case R.id.ll_hand_register:
                 cmd = "03";
                 break;
-            case R.id.hand_identification:
+            case R.id.ll_hand_identification:
                 cmd = "04";
                 break;
-            case R.id.hand_open:
+            case R.id.ll_hand_open:
                 if (!openFlag) {//尚未开启
                     cmd = "05";
                 } else {
@@ -107,19 +129,19 @@ public class PageHandFragment extends BaseHomeFragment {
     private void sendCmdByGPRS(View v) {
         String cmd = null;
         switch (v.getId()) {
-            case R.id.hand_delete:
+            case R.id.ll_hand_delete:
                 cmd = "DeleteOne";
                 break;
-            case R.id.hand_deleteall:
+            case R.id.ll_hand_deleteall:
                 cmd = "DeleteAll";
                 break;
-            case R.id.hand_register:
+            case R.id.ll_hand_register:
                 cmd = "EnrollFinger";
                 break;
-            case R.id.hand_identification:
+            case R.id.ll_hand_identification:
                 cmd = "IdentifyFinger";
                 break;
-            case R.id.hand_open:
+            case R.id.ll_hand_open:
                 if (!openFlag) {//尚未开启
                     openFlag = true;
                     cmd = "OpenFinger";
@@ -134,12 +156,131 @@ public class PageHandFragment extends BaseHomeFragment {
         sendDataByGPRS(cmd);
     }
 
-    public void updateCmdStatus(String msg) {
-        handShowstatus.setText(msg);
+    private void sendCmdByNetwork(View view) {
+        String cmd = null;
+        switch (view.getId()) {
+            case R.id.ll_hand_delete:
+                cmd = "DeleteOne";
+                break;
+            case R.id.ll_hand_deleteall:
+                cmd = "DeleteAll";
+                break;
+            case R.id.ll_hand_register:
+                cmd = "EnrollFinger";
+                break;
+            case R.id.ll_hand_identification:
+                cmd = "IdentifyFinger";
+                break;
+            case R.id.ll_hand_open:
+                if (!openFlag) {//尚未开启
+                    openFlag = true;
+                    cmd = "OpenFinger";
+                    handOpen.setBackgroundDrawable(closeStateListDrawable);
+                } else {
+                    openFlag = false;
+                    cmd = "CloseFinger";
+                    handOpen.setBackgroundDrawable(openStateListDrawable);
+                }
+                break;
+        }
+        submitTask(cmd,null);
     }
 
-    @Override
-    protected void updateHandAreaSucess(int cmd) {
+    class CustomInterpolator implements TimeInterpolator{
+        @Override
+        public float getInterpolation(float input){
+            input *= 0.8f;
+            return input*input;
+        }
+    }
+
+    /**
+     * 使用ValueAnimator实现图片缩放动画
+     */
+    public void setAnimator(View view){
+        ImageView imageView = null;
+        switch(view.getId()){
+            case R.id.ll_hand_delete:
+                imageView = handDelete;
+                break;
+            case R.id.ll_hand_deleteall:
+                imageView = handDeleteall;
+                break;
+            case R.id.ll_hand_register:
+                imageView = handRegister;
+                break;
+            case R.id.ll_hand_identification:
+                imageView = handIdentification;
+                break;
+            case R.id.ll_hand_open:
+                imageView = handOpen;
+                break;
+        }
+        scaleValueAnimator(imageView);
+    }
+
+    //使用ValueAnimator实现图片缩放
+    private void scaleValueAnimator(final ImageView imageView){
+        //1.设置目标属性名及属性变化的初始值和结束值
+        PropertyValuesHolder propertyValuesHolderScaleX = PropertyValuesHolder.ofFloat("scaleX",1.0f,0.0f);
+        PropertyValuesHolder propertyValuesHolderScaleY = PropertyValuesHolder.ofFloat("scaleY",1.0f,0.0f);
+        ValueAnimator mAnimator = ValueAnimator.ofPropertyValuesHolder(propertyValuesHolderScaleX,propertyValuesHolderScaleY);
+        //2.为目标对象的属性变化设置监听器
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //3.根据属性名变化的值分别为ImageView目标
+                float animatorValueScaleX = (float)animation.getAnimatedValue("scaleX");
+                float animatorValueScaleY = (float)animation.getAnimatedValue("scaleY");
+                imageView.setScaleX(animatorValueScaleX);
+                imageView.setScaleY(animatorValueScaleY);
+            }
+        });
+        //4.为ValueAnimator设置自定义的Interpolator
+        mAnimator.setInterpolator(new CustomInterpolator());
+        //5.设置动画持续时间、是否重复以及重复次数等属性
+        mAnimator.setDuration(200);
+        mAnimator.setRepeatCount(1);
+        mAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        //6.位ValueAnimator设置目标对象并开始执行动画
+        mAnimator.setTarget(imageView);
+        mAnimator.start();
+    }
+
+    public void setShowAreaBT(String text){
+        if(text.startsWith("EB 90 00 ")){
+            int start = 0;
+            for(int i=0;i<3;i++){
+                start = start+3;
+            }
+            text = text.substring(start,start+2);
+            if(text.startsWith("8")){
+                int cmd = Integer.parseInt(text.substring(1),16);
+                FutureTask<Integer> futureTask = mBluetoothMap.get("0"+cmd);
+                if(futureTask!=null && !futureTask.isDone()){
+                    futureTask.cancel(true);
+                }
+                else{//没进去
+                }
+                updateArea(cmd);
+            }
+        }
+        else if(text.startsWith("EB 90 02 ")){
+            int start = 0;
+            for(int i=0;i<3;i++){
+                start = start+3;
+            }
+            String cmd = text.substring(start,start+2);
+            String hexHigh = text.substring(start+3,start+5);
+            String hexLow = text.substring(start+6,start+8);
+            int num = Integer.parseInt(hexHigh+hexLow,16);
+            if(cmd.startsWith("8")){
+                updateArea(cmd,num);
+            }
+        }
+    }
+
+    protected void updateArea(int cmd) {
         switch (cmd) {
             case 1:
                 handShowstatus.setText("指令:" + "删除单个指纹·····" + "发送成功");
@@ -166,8 +307,11 @@ public class PageHandFragment extends BaseHomeFragment {
         }
     }
 
-    @Override
-    protected void updateHandAreaSucessState(String cmd, int num) {
+    public void updateArea(String msg) {
+        handShowstatus.setText(msg);
+    }
+
+    public void updateArea(String cmd, int num) {
         switch (Integer.parseInt(cmd.substring(1), 16)) {
             case 1:
                 handShowstatus.setText("删除单个指纹成功后,当前指纹个数为:" + num);
@@ -175,6 +319,15 @@ public class PageHandFragment extends BaseHomeFragment {
             case 3:
                 handShowstatus.setText("注册指纹指令执行后,当前指纹个数为:" + num);
                 break;
+        }
+    }
+
+    public void setShowAreaNetwork(String data){
+        updateArea("服务器返回数据为:"+data);
+        String cmd = data.substring(2);
+        FutureTask<Integer> futureTask = mNetWorkMap.get(cmd);
+        if(futureTask!=null && !futureTask.isDone()){
+            futureTask.cancel(true);
         }
     }
 }
