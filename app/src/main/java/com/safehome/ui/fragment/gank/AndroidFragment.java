d6e290448d0bf0b2298a6de09dc1ba386af330be
XiDianMouse <xidian_guobenhao@163.com>
2017-05-24 11:44:30 +0800

    用于显示从Gnak获取到的数据的Fragment

--- app/src/main/java/com/safehome/ui/fragment/gank/AndroidFragment.java
+++ app/src/main/java/com/safehome/ui/fragment/gank/AndroidFragment.java
@@ -4,18 +4,18 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.support.v7.widget.LinearLayoutManager;
 
-import com.blankj.utilcode.utils.ToastUtils;
 import com.safehome.R;
 import com.safehome.adapter.GankIoAndroidAdapter;
 import com.safehome.bean.gankio.GankIoDataBean;
 import com.safehome.bean.gankio.GankIoDataBean.ResultBean;
-import com.safehome.injector.component.fragment.DaggerAndroidComponent;
+import com.safehome.injector.component.DaggerAndroidComponent;
 import com.safehome.injector.module.fragment.AndroidModule;
 import com.safehome.injector.module.http.GankIoHttpModule;
 import com.safehome.listeners.OnItemClickListener;
 import com.safehome.presenter.GankIoAndroidPresenter.View;
 import com.safehome.presenter.impl.GankIoAndroidPresenterImpl;
 import com.safehome.ui.fragment.BaseFragment;
+import com.safehome.webview.WebViewActivity;
 
 import java.util.List;
 
@@ -28,7 +28,8 @@ import butterknife.BindView;
  */
 
 public class AndroidFragment extends BaseFragment<GankIoAndroidPresenterImpl>
-        implements View, OnRefreshListener, OnItemClickListener<ResultBean>{
+        implements View, OnRefreshListener,
+            OnItemClickListener<ResultBean>,AutoLoadRecyclerView.OnLoadListener{
 
     @BindView(R.id.rv_android)
     AutoLoadRecyclerView rvAndroid;
@@ -50,10 +51,11 @@ public class AndroidFragment extends BaseFragment<GankIoAndroidPresenterImpl>
 
     @Override
     public void initView() {
-        srlAndroid.setColorSchemeColors(getResources().getColor(R.color.colorTheme));
-        srlAndroid.setOnRefreshListener(this);
+        srlAndroid.setColorSchemeColors(getResources().getColor(R.color.colorTheme));//设置进度动画的颜色
+        srlAndroid.setOnRefreshListener(this);//设置手势滑动监听器
         mGankIoAndroidAdapter = (GankIoAndroidAdapter)mAdapter;
         mGankIoAndroidAdapter.setItemClickListener(this);
+        rvAndroid.setOnLoadListener(this);
         rvAndroid.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false));
         rvAndroid.setAdapter(mGankIoAndroidAdapter);
     }
@@ -78,7 +80,7 @@ public class AndroidFragment extends BaseFragment<GankIoAndroidPresenterImpl>
     @Override
     public void refreshView(List<ResultBean> data) {
         if (isRefresh) {
-            srlAndroid.setRefreshing(false);
+            srlAndroid.setRefreshing(false);//设置组件的刷新状态,false:关闭刷新,否则刷新球一直在转
             isRefresh = false;
             mGankIoAndroidAdapter.addItems(data);
         } else {
@@ -90,6 +92,12 @@ public class AndroidFragment extends BaseFragment<GankIoAndroidPresenterImpl>
 
     @Override
     public void onClick(GankIoDataBean.ResultBean item) {
-        ToastUtils.showShortToast(item.getUrl());
+        WebViewActivity.loadUrl(getActivity(),item.getUrl(),item.getSource());
+    }
+
+    @Override
+    public void onLoad(){
+        loadData();
+        srlAndroid.setEnabled(false);//控制是否可以刷新
     }
 }
