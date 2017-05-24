00647366664386aae04754f5a19e1c98e08c05fa
XiDianMouse <xidian_guobenhao@163.com>
2017-05-24 11:36:19 +0800

    Gank网络请求Module

--- app/src/main/java/com/safehome/injector/module/http/GankIoHttpModule.java
+++ app/src/main/java/com/safehome/injector/module/http/GankIoHttpModule.java
@@ -3,7 +3,6 @@ package com.safehome.injector.module.http;
 
 import com.safehome.http.service.GankIoService;
 import com.safehome.http.utils.RetrofitGankIoUtils;
-import com.safehome.injector.qualifier.GankUrl;
 
 import javax.inject.Singleton;
 
@@ -16,10 +15,6 @@ import retrofit2.Retrofit;
  * Created on 2017/5/7.
  */
 
-/*
-* @Module:Module类里面的方法专门提供依赖，所以我们定义一个类，用@Module注解，
-* 这样Dagger在构造类的实例的时候，就知道从哪里去找到需要的依赖。
-* */
 @Module
 public class GankIoHttpModule extends BaseHttpModule {
 
@@ -28,14 +23,13 @@ public class GankIoHttpModule extends BaseHttpModule {
     * */
     @Singleton
     @Provides
-    @GankUrl
     Retrofit provideGankIoRetrofit(Retrofit.Builder builder, OkHttpClient client) {
         return createRetrofit(builder, client, GankIoService.API_GANKIO);
     }
 
     @Singleton
     @Provides
-    GankIoService provideGankIoService(@GankUrl Retrofit retrofit) {
+    GankIoService provideGankIoService(Retrofit retrofit) {
         return retrofit.create(GankIoService.class);
     }
 
