016b8893a32b30ed94901404936fb9ae4b6eddfe
XiDianMouse <xidian_guobenhao@163.com>
2017-05-24 11:13:16 +0800

    获取短信发送状态

--- app/src/main/java/com/safehome/gprs/SmsReceiver.java
+++ app/src/main/java/com/safehome/gprs/SmsReceiver.java
@@ -8,22 +8,16 @@ import android.widget.Toast;
 
 import com.safehome.gprs.PhoneControl.Content;
 
-public class SmsReceiver extends BroadcastReceiver
-{
+public class SmsReceiver extends BroadcastReceiver {
 	@Override
-	public void onReceive(Context context, Intent intent) 
-	{
-		if(Content.SEND_SMS_ACTION.equals(intent.getAction()))
-		{
+	public void onReceive(Context context, Intent intent) {
+		if(Content.SEND_SMS_ACTION.equals(intent.getAction())) {
 			if(getResultCode()==Activity.RESULT_OK){//短信发送成功
 				Toast.makeText(context, "短信发送成功", Toast.LENGTH_LONG).show();
-			}
-			else
-			{
+			} else {
 				Toast.makeText(context, "短信发送失败", Toast.LENGTH_LONG).show();
 			}
-		}else if(Content.DELIVERED_SMS_ACTION.equals(intent.getAction()))
-		{
+		}else if(Content.DELIVERED_SMS_ACTION.equals(intent.getAction())) {
 			Toast.makeText(context, "收件人已成功接收", Toast.LENGTH_LONG).show();
 		}
 	}
